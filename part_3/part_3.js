15affdd9fb48cbac8e40dda9f97f5dafd43e90d6
Mohammad-ghazal <moh.g.ghazal@gmail.com>
2021-09-05 20:57:17 +0300

    solve part 3

--- part_3/part_3.js
+++ part_3/part_3.js
@@ -8,11 +8,40 @@ return 'YES' if the parentheses in the input string are balanced,
 return 'NO' if the parentheses in the input string are not balanced,. 
 
 */
+const balance = {
+  '(': ')',
+  '{': '}',
+  '[': ']',
+  ')': '(',
+  '}': '{',
+  ']': '['
+}
+
 
 console.log('PART 4');
 
-const isABalancedString = () => {
-  // WRITE YOUR CODE BELOW THIS LINE
+const isABalancedString = (string) => {
+
+  let length = string.length;
+  console.log('length :', length);
+
+
+  let b =
+    string.split('').splice(0, length / 2).map((item) => {
+      return balance[item]
+
+    });
+  console.log(string.split('').splice(length / 2, length).reverse().join(''));
+  console.log(b.join(''));
+
+
+
+
+  if (string.split('').splice(length / 2, length).reverse().join('') === b.join(''))
+    return true;
+
+  else return false;
+
 };
 
 /*
